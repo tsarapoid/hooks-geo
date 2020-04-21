22180ae86e6eeea836d1bedb68221dd5de0c2c09
tsarapoid <nadein@gmail.com>
2020-04-21 10:12:47 +0300

    add geolocation

---
+++ src/Demo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {usePosition} from '../src/usePosition';
+
+export const Demo = ({watch, settings}) => {
+  const {
+    latitude,
+    longitude,
+    timestamp,
+    accuracy,
+    error,
+  } = usePosition(watch, settings);
+
+  const loader = !latitude && !error ? (
+    <>
+      <div>Trying to fetch location...</div>
+      <br/>
+    </>
+  ) : null;
+
+  return (
+    <>
+      {loader}
+      <code>
+        latitude: {latitude}<br/>
+        longitude: {longitude}<br/>
+        timestamp: {timestamp}<br/>
+        accuracy: {accuracy && `${accuracy}m`}<br/>
+        error: {error}
+      </code>
+    </>
+  );
+};
+
+Demo.propTypes = {
+  watch: PropTypes.bool,
+  settings: PropTypes.object,
+};
+
+export default Demo;
