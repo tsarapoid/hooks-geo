af36a040a1d524b113ffa364d7086dfa7f459259
tsarapoid <nadein@gmail.com>
2020-04-22 21:14:04 +0300

    Updates

---
+++ precache-manifest.2dcacf8ff98e06bfa40d1b49ab024b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1afc421b685015561bac54a9b74794",
+    "url": "/hooks-geo/index.html"
+  },
+  {
+    "revision": "cb661cd56576772a9f6a",
+    "url": "/hooks-geo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e81a6e701210f07554c6",
+    "url": "/hooks-geo/static/js/2.0d34a311.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks-geo/static/js/2.0d34a311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb661cd56576772a9f6a",
+    "url": "/hooks-geo/static/js/main.1dbeee76.chunk.js"
+  },
+  {
+    "revision": "399474f447441af4c462",
+    "url": "/hooks-geo/static/js/runtime-main.8a91a3c8.js"
+  }
+]);
